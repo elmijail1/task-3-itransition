7fab0f8eaf8b0f2a3bae81c4623a597455a12eef
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-20 13:00:51 +0400

    Finish verification for the initial arguments

--- game.js
+++ game.js
@@ -25,41 +25,58 @@ if (process.argv.length < 5) {
   console.error(
     "You haven't specified enough dice. Please specify at least 3 dice."
   );
-  return;
+  process.exit();
 }
 
 // dice verification
 let dice = process.argv.slice(2).map((arg, index) => {
   let die = { die: arg.split(","), initialIndex: index };
-  console.log(die);
+
   if (die.die.length !== 6) {
+    console.log(`Error at:
+    Dice: ${die.die}
+`);
     console.error(
       "One of your dice has the number of faces other than 6. Please specify 6 faces (values) for each of your dice."
     );
-    return;
+    process.exit();
   }
-  for (let i = 0; i < die.length; i++) {
-    if (die[i].isNaN()) {
+
+  for (let i = 0; i < die.die.length; i++) {
+    if (isNaN(die.die[i])) {
+      console.log(`Error at:
+    Dice: ${die.die}
+    Face: ${die.die[i]}
+    Index: ${i}
+`);
       console.error(
         "One of your dice has a value other than a number on one of its faces. Please specify 6 integers for each of your dice."
       );
-      return;
-    } else if (die[i] < 0) {
+      process.exit();
+    } else if (die.die[i] < 0) {
+      console.log(`Error at:
+    Dice: ${die.die}
+    Face: ${die.die[i]}
+    Index: ${i}
+`);
       console.error(
         "One of your dice has a negative value on one of its faces. Please specify 6 positive integers for each of your dice."
       );
-      return;
-    } else if (!Number.isInteger(die[i])) {
+      process.exit();
+    } else if (!Number.isInteger(Number(die.die[i]))) {
+      console.log(`Error at:
+    Dice: ${die.die}
+    Face: ${die.die[i]}
+    Index: ${i}
+`);
       console.error(
         "One of your dice has a non-integer value on one of its faces. Please specify 6 integers for each of your dice."
       );
-      return;
+      process.exit();
     }
   }
   return die;
 });
-
-console.log(dice);
 
 // prog chooses a random die and it's removed from the dice
 function chooseARandomDie() {
