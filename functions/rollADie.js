d6d765e3760da229bb443ba0380c380a8754d1b8
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-20 12:41:00 +0400

    Remove redundancy
    Dice now have an array for its die property

--- functions/rollADie.js
+++ functions/rollADie.js
@@ -1,13 +1,11 @@
 exports.rollADie = (die) => {
-  die = die.split(",");
   return die[Math.floor(Math.random() * die.length)];
 };
 
 {
   /*
   ## What does it do?
-- Accepts a die as a string and splits it into an array
-- Randomly picks one of the values
+  - Randomly picks one of the values
   ,
   ## How is it used?
   - **Where?** Sections 3-4
