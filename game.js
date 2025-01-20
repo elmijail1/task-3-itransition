531917095be510459ef03c2471f847931c635031
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-20 10:40:20 +0400

    Add logic for the first roll

--- game.js
+++ game.js
@@ -22,6 +22,51 @@ function chooseARandomDie() {
   return randomDie;
 }
 
+function generateAProof(whoRolls) {
+  let rollerPronoun;
+  if (whoRolls === "player") {
+    rollerPronoun = "you";
+  } else {
+    rollerPronoun = "me";
+  }
+
+  return new Promise((resolve, reject) => {
+    inputReader.question(
+      `It's time for ${rollerPronoun} to roll.
+I selected a random value in the range from 0 to 5.
+(HMAC = ...)
+Add your number modulo 6:
+– Enter "0" for 0
+- Enter "1" for 1
+- Enter "2" for 2
+- Enter "3" for 3
+- Enter "4" for 4
+- Enter "5" for 5
+  - - -
+- Enter "x" to exit
+- Enter "?" for help
+`,
+      (answer) => {
+        if (["0", "1", "2", "3", "4", "5", "x", "?"].includes(answer)) {
+          if (["0", "1", "2", "3", "4", "5"].includes(answer)) {
+            resolve(answer);
+          } else if (answer === "?") {
+            console.log("Help is on the way!");
+          } else if (answer === "x") {
+            process.exit();
+          }
+        } else {
+          reject(
+            "You've entered a non-existant option. Try again: choose among 0, 1, 2, 3, 4, 5, x, and ?."
+          );
+        }
+      }
+    );
+  });
+}
+
+// * * * * *
+
 // M A I N  F U N C T I O N
 async function main() {
   const randomNumberFrom0To1 = Math.round(Math.random());
@@ -38,14 +83,23 @@ async function main() {
   // key calculation
   //   who's first to roll calculation
   let firstRoller;
+  let firstRollerPronoun;
+  let secondRoller;
+  let secondRollerPronoun;
   console.log(response1, randomNumberFrom0To1);
 
   function firstToRoll() {
     if (Number(response1) === randomNumberFrom0To1) {
       firstRoller = "player";
+      firstRollerPronoun = "your";
+      secondRoller = "program";
+      secondRollerPronoun = "my";
       return "you";
     } else {
       firstRoller = "program";
+      firstRollerPronoun = "my";
+      secondRoller = "player";
+      secondRollerPronoun = "your";
       return "me";
     }
   }
@@ -67,7 +121,8 @@ Hence, first to roll is ${firstToRoll()}!
   // QUESTION 2 SECTION
   try {
     var diePlayers = await chooseADie(inputReader, dice); // the die you've chosen
-    console.log(`You chose this die: ${diePlayers.die}`); // display the chosen die
+    console.log(`You chose this die: ${diePlayers.die}
+`); // display the chosen die
     dice = dice.filter((die) => die.initialIndex !== diePlayers.initialIndex); // remove the die you've chosen from the dice array
   } catch (error) {
     console.error(error);
@@ -77,6 +132,39 @@ Hence, first to roll is ${firstToRoll()}!
   // * *
   // * * *
   // QUESTION 3 SECTION
+  let progsRandomNumForKey = Math.floor(Math.random() * 6);
+
+  function rollADie(die) {
+    die = die.split(",");
+    return die[Math.floor(Math.random() * die.length)];
+  }
+
+  if (firstRoller === "player") {
+    var firstRollResult = rollADie(diePlayers.die);
+  } else {
+    var firstRollResult = rollADie(dieProgs.die);
+  }
+
+  try {
+    var firstRollProof = await generateAProof(firstRoller); // player's part of the proof
+    console.log(`
+You chose this number: ${firstRollProof}
+I chose this number: ${progsRandomNumForKey}
+(KEY = ...)
+The result is ${firstRollProof} + ${progsRandomNumForKey} = ${
+      (Number(firstRollProof) + progsRandomNumForKey) % 6
+    }
+
+${
+  firstRollerPronoun[0].toUpperCase() + firstRollerPronoun.slice(1)
+} roll is ${firstRollResult}.
+
+`); // display the returned number
+  } catch (error) {
+    console.error(error);
+    process.exit();
+  }
+
   // dieProgs is rolled
 
   // * *
