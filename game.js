5ccc293fb0c6e2ec3dd2415f0d2b2530e48205d4
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-20 13:07:19 +0400

    Move out initial input processing & verification

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 // General
 const { inputReader } = require("./utilities/inputReader.js");
+const { processInitInput } = require("./functions/processInitInput.js");
 // For question 1
 const determineWhoRollsFirst = require("./functions/determineWhoRollsFirst.js");
 // For question 2
@@ -21,62 +22,7 @@ const { determineWinner } = require("./functions/determineWinner.js");
 // 2.2. The help table (use APIs)
 // 2.3. If there's still some time left, see if you can add OOP to it all somehow
 
-if (process.argv.length < 5) {
-  console.error(
-    "You haven't specified enough dice. Please specify at least 3 dice."
-  );
-  process.exit();
-}
-
-// dice verification
-let dice = process.argv.slice(2).map((arg, index) => {
-  let die = { die: arg.split(","), initialIndex: index };
-
-  if (die.die.length !== 6) {
-    console.log(`Error at:
-    Dice: ${die.die}
-`);
-    console.error(
-      "One of your dice has the number of faces other than 6. Please specify 6 faces (values) for each of your dice."
-    );
-    process.exit();
-  }
-
-  for (let i = 0; i < die.die.length; i++) {
-    if (isNaN(die.die[i])) {
-      console.log(`Error at:
-    Dice: ${die.die}
-    Face: ${die.die[i]}
-    Index: ${i}
-`);
-      console.error(
-        "One of your dice has a value other than a number on one of its faces. Please specify 6 integers for each of your dice."
-      );
-      process.exit();
-    } else if (die.die[i] < 0) {
-      console.log(`Error at:
-    Dice: ${die.die}
-    Face: ${die.die[i]}
-    Index: ${i}
-`);
-      console.error(
-        "One of your dice has a negative value on one of its faces. Please specify 6 positive integers for each of your dice."
-      );
-      process.exit();
-    } else if (!Number.isInteger(Number(die.die[i]))) {
-      console.log(`Error at:
-    Dice: ${die.die}
-    Face: ${die.die[i]}
-    Index: ${i}
-`);
-      console.error(
-        "One of your dice has a non-integer value on one of its faces. Please specify 6 integers for each of your dice."
-      );
-      process.exit();
-    }
-  }
-  return die;
-});
+let dice = processInitInput(process.argv);
 
 // prog chooses a random die and it's removed from the dice
 function chooseARandomDie() {
